2e8e8da8d5c20de337b8ac69ea7439f5d378ec93
ieidsath <ieidsath@gmail.com>
2013-01-27 22:23:18 -0700

    Exercise 1

---
+++ Exercise 1/ex1.py
@@ -0,0 +1,13 @@
+#print "Hello World!"
+#print "Hello Again!"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+
+print "This should be the only line that prints."
+
+#print "This line is invisible."
+
+
